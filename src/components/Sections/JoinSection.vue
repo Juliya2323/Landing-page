d155f19b9c1d252806ba64c9a81079ec08d9b91b
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-01-05 19:42:22 +0300

    add positions for divs

--- src/components/Sections/JoinSection.vue
+++ src/components/Sections/JoinSection.vue
@@ -1,27 +1,26 @@
 <template>
     <div class="join">
-        <div class="join_background">
-            <ul class="join_background_lines">
-                <li class="join_background_lines_item">
+            <ul class="join_sections">
+                <li class="join_sections_item">
                     <img :src="firstLineImg" alt="img" v-for="firstLineImg in firstLineImgs">
                 </li>
-                <li class="join_background_lines_item center">
-                    <img :src="secondLinImg" alt="img" v-for="secondLinImg in secondLinImgs">
+                <li class="join_sections_item center">
+                    <div class="join_sections_item_img"><img :src="imgLink3" alt="img"></div>
+                    <div class="join_sections_item_info">
+                        <div class="join_sections_item_info_title">Join our</div>
+                        <div class="join_sections_item_info_subtitle">Community</div>
+                        <div class="join_sections_item_info_description">Meet the Omise community,artists and collectors for platform update,announcements and more...</div>
+                        <base-button class="join_sections_item_info_button">
+                            <img class="join_sections_item_info_button_icon" :src="Discord" alt="svg">
+                            <div class="join_sections_item_info_button_text">Launch Discord</div>
+                        </base-button>
+                    </div>
+                    <div class="join_sections_item_img"><img :src="imgLink4" alt="img"></div>
                 </li>
-                <li class="join_background_lines_item">
+                <li class="join_sections_item">
                     <img :src="thirdLineImg" alt="img" v-for="thirdLineImg in thirdLineImgs">
                 </li>
             </ul>
-        </div>
-        <div class="join_info">
-            <div class="join_info_title">Join our</div>
-            <div class="join_info_subtitle">Community</div>
-            <div class="join_info_description">Meet the Omise community,artists and collectors for platform update,announcements and more...</div>
-            <base-button class="join_info_button">
-                <img class="join_info_button_icon" :src="Discord" alt="svg">
-                <div class="join_info_button_text">Launch Discord</div>
-            </base-button>
-        </div>
     </div>
 </template>
 
@@ -36,7 +35,7 @@ import Discord from '@/assets/icons/discord.svg';
 
 
 const firstLineImgs = [imgLink1, imgLink2];
-const secondLinImgs = [imgLink3, imgLink4];
+/* const secondLinImgs = [imgLink3, imgLink4]; */
 const thirdLineImgs = [imgLink5, imgLink6];
 
 </script>
@@ -50,74 +49,71 @@ const thirdLineImgs = [imgLink5, imgLink6];
         color: #FFFFFF;
         position: relative;
 
-        &_background {
-
-            &_lines {
-                list-style: none;
-                display: flex;
-                flex-direction: column;
-                padding: 0;
-                margin: 0;
-                gap: 125px;
-
-                &_item {
-                    display: flex;
-                    justify-content: space-around;
-                }
-
-                &_item.center {
-                    display: flex;
-                    justify-content: space-between;
-                }
-            }
-        }
-
-        &_info {
+        &_sections {
+            list-style: none;
             display: flex;
             flex-direction: column;
-            align-items: center;
-            position: absolute;
-            left: 24%;
-            top: 19%;
+            padding: 0;
+            margin: 0;
 
-
-            &_title {
-                font-weight: 400;
-                font-size: 36px;
-                line-height: 41px;
-                margin-bottom: 5px;
-            }
-
-            &_subtitle {
-                font-weight: 700;
-                font-size: 36px;
-                line-height: 41px;
-                color: #5BA300;
-                margin-bottom: 31px;
-            }
-
-            &_description {
-                font-weight: 400;
-                font-size: 22px;
-                line-height: 29px;
-                width: 70%;
-                text-align: center;
-                margin-bottom: 67px;
-            }
-
-            &_button {
-
+            &_item {
                 display: flex;
-                align-items:center;
-                padding: 20px 29px;
-                gap: 12px;
+                justify-content: space-around;
 
-                &_text {
-                    font-weight: 400;
-                    font-size: 14.0373px;
-                    line-height: 16px;
+                &_info {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+
+
+                    &_title {
+                        font-weight: 400;
+                        font-size: 36px;
+                        line-height: 41px;
+                        margin-bottom: 5px;
+                    }
+
+                    &_subtitle {
+                        font-weight: 700;
+                        font-size: 36px;
+                        line-height: 41px;
+                        color: #5BA300;
+                        margin-bottom: 31px;
+                    }
+
+                    &_description {
+                        font-weight: 400;
+                        font-size: 22px;
+                        line-height: 29px;
+                        width: 70%;
+                        text-align: center;
+                        margin-bottom: 67px;
+                    }
+
+                    &_button {
+
+                        display: flex;
+                        align-items:center;
+                        padding: 20px 29px;
+                        gap: 12px;
+
+                        &_text {
+                            font-weight: 400;
+                            font-size: 14.0373px;
+                            line-height: 16px;
+                        }
+                    }
                 }
             }
+
+            &_item.center {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+            }
         }
+        
+
+        
     }
 </style>
