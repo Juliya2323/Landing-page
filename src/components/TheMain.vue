fa5efc8cbc89b8fd2cd79cf1e7f1b592bf495ad4
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-01-05 19:20:15 +0300

    add join section

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -2,6 +2,7 @@
     <primary-section></primary-section>
     <gallery-section></gallery-section>
     <creators-section></creators-section>
+    <join-section></join-section>
 </template>
 
 
@@ -9,5 +10,6 @@
     import PrimarySection from '@/components/sections/PrimarySection.vue';
     import GallerySection from '@/components/sections/GallerySection.vue';
     import CreatorsSection from '@/components/sections/CreatorsSection.vue';
+    import JoinSection from '@/components/sections/JoinSection.vue';
 </script>
 
